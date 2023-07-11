004f3cc5e728b1a2aa93aad3261f37e87a7f008a
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-10 22:21:35 -0300

    refactor: :zap: add request in postUser and loginUser

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,21 @@
 const { request, response } = require('express');
 
 const postUser = (req = request, res = response) => {
-	res.json({ ok: true, msg: 'register' });
+	const { name, email, password } = req.body;
+
+	return res
+		.status(201)
+		.json({ ok: true, msg: 'register', name, email, password });
 };
 
 const loginUser = (req = request, res = response) => {
-	res.json({ ok: true, msg: 'login' });
+	const { email, password } = req.body;
+
+	return res.status(200).json({ ok: true, msg: 'login', email, password });
 };
 
 const renewToken = (req = request, res = response) => {
-	res.json({ ok: true, msg: 'renew' });
+	return res.json({ ok: true, msg: 'renew' });
 };
 
 module.exports = {
