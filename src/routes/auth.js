73d5a82521f761ee893aa2eccfd2b786a17e9d0b
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-10 22:20:49 -0300

    refactor: :zap: add middlewares at postUser & loginUser

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,45 @@
 /*
-  Rutas de Usuarios / Auth
-  host:port/api/auth
-  example: http://localhost:8080/api/auth
+   Rutas de Usuarios / Auth
+   <host>:<port>/api/auth
+   example: http://localhost:8080/api/auth
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { fieldValidators } = require('../middlewares/fieldValidators');
 const router = Router();
 
 const { postUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/', loginUser);
+router.post(
+	'/new',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check(
+			'name',
+			'the name must be greater than or equal to 3 characters'
+		).isLength({ min: 3 }),
+		check('email', 'You must provide an email').isEmail(),
+		check('password', 'Password is required').not().isEmpty(),
+		check('password', 'Password must be longer than 6 characters').isLength({
+			min: 6,
+		}),
+		fieldValidators,
+	],
+	postUser
+);
 
-router.post('/new', postUser);
+router.post(
+	'/',
+	[
+		check('email', 'You must provide an email').isEmail(),
+		check('password', 'Password is required').not().isEmpty(),
+		check('password', 'Password must be longer than 6 characters').isLength({
+			min: 6,
+		}),
+		fieldValidators,
+	],
+	loginUser
+);
 
 router.get('/renew', renewToken);
 
